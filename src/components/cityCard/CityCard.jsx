b66493ec4e775b888bead1d70b9163522ac01a45
Baljinder-Singh <baljindersingh987209@gmail.com>
2025-04-21 08:57:52 +0530

    fix: update city names and images in CityCard component

--- src/components/cityCard/CityCard.jsx
+++ src/components/cityCard/CityCard.jsx
@@ -26,15 +26,15 @@ function CityCard() {
         <div>
           <img
             className="h-auto w-full object-cover"
-            src="https://www.w3schools.com/w3images/newyork.jpg"
+            src="https://www.w3schools.com/w3images/sanfran.jpg"
             alt="New York"
           />
         </div>
         <div className="p-4">
-          <div className="text-lg font-bold mt-2 text-black ">New York</div>
-          <div className="text-sm py-5 text-gray-700 ">Fri 27 Nov 2016</div>
+          <div className="text-lg font-bold mt-2 text-black ">Paris</div>
+          <div className="text-sm py-5 text-gray-700 ">Sat 28 Nov 2016</div>
           <div className="text-sm  mt-2 text-black">
-            Praesent tincidunt sed tellus ut rutrum sed vitae justo.
+          Praesent tincidunt sed tellus ut rutrum sed vitae justo.
           </div>
           <button className="mt-4 bg-black text-white py-2 px-4 rounded hover:bg-gray-400">
             Buy Tickets
@@ -45,15 +45,15 @@ function CityCard() {
         <div>
           <img
             className="h-auto w-full object-cover"
-            src="https://www.w3schools.com/w3images/newyork.jpg"
+            src="https://www.w3schools.com/w3images/paris.jpg"
             alt="New York"
           />
         </div>
         <div className="p-4">
-          <div className="text-lg font-bold mt-2 text-black ">New York</div>
-          <div className="text-sm py-5 text-gray-700 ">Fri 27 Nov 2016</div>
+          <div className="text-lg font-bold mt-2 text-black ">San Francisco</div>
+          <div className="text-sm py-5 text-gray-700 ">Sun 29 Nov 2016</div>
           <div className="text-sm  mt-2 text-black">
-            Praesent tincidunt sed tellus ut rutrum sed vitae justo.
+          Praesent tincidunt sed tellus ut rutrum sed vitae justo.
           </div>
           <button className="mt-4 bg-black text-white py-2 px-4 rounded hover:bg-gray-400">
             Buy Tickets
